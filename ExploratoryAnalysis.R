afee8a45ae8ffa38d5b61c51298c732ae069ab71
Morgan Thomas <morgan.thomas@students.plymouth.ac.uk>
2026-04-21 16:02:21 +0100

    Experimenting with converting age band codes into factors

--- ExploratoryAnalysis.R
+++ ExploratoryAnalysis.R
@@ -9,6 +9,7 @@ reduced_as_table <- table(reduced_data$AAGE, reduced_data$CIGEVER)
 
 reduced_as_table <- reduced_as_table[-(1:2),]
 
+#Chi-Square test
 chisq.test(reduced_as_table)
 
 head(reduced_as_table, n=15)
