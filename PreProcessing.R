25e4380251f1b09c49e982b8ded970ad2ba66c16
Morgan Thomas <morgan.thomas@students.plymouth.ac.uk>
2026-04-30 14:46:01 +0100

    pre processing plans

---
+++ PreProcessing.R
@@ -0,0 +1,29 @@
+
+##load csvs
+
+##clean names (janitor)
+
+##get uniques to 1921 // 2224, then common (interesect, setdiffs)
+
+## scaffold:
+  # handle codes (almost all -> na)
+  # factor conversion (-8/-9 -> na)
+  # numeric conversion (99995~ -> na)
+
+# basic demographics
+# geography (region/county + lad?)
+
+# ILO emp status
+# full vs part
+# perm vs temp
+# usual weekly hrs
+# industry sector (SIC)
+# occupation (SOC) 
+
+# education (highest qual, whether in education, apprenticeship, training)
+# health + disabilities
+# household (income, own/rent, earners, children)
+# weightings
+
+
+# then redraft rpt struct
